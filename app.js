3efbd7dae93cf3fb5a0daf47eb189ad008b82066
Mihidari <theo.lanique@gmail.com>
2019-09-06 15:59:11 +0200

    socket-server add

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ let server = app.listen(process.env.PORT, () => {
   console.log(`Le server est ok sur le port ${process.env.PORT}`);
 });
 
+let io = socketIo(server);
+
 io.on('connection', (socket)=>{
     socket.on('vote', (data) => {
         //TODO: Si la barre de soulance est au max on switch de vidéo
