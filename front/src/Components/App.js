0127c68ecf48cf35f1b7a7062aa00ffe741be580
Quentin <quentin.senelas@gmail.com>
2019-09-17 10:09:39 +0200

    Premières intégrations front

--- front/src/Components/App.js
+++ front/src/Components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './header';
+import Elements from './elements';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Elements />
     </div>
   );
 }
